63c3f7f0a3835cd306db5dc11e5f92d1d48df3d3
seuem <lazyslug@kakao.com>
2024-12-12 22:04:39 +0900

    57분 정답다시보고 고치기

--- Inflearn/해싱And시간파싱/음수가있는부분수열.java
+++ Inflearn/해싱And시간파싱/음수가있는부분수열.java
@@ -6,9 +6,9 @@ public class 음수가있는부분수열 {
     public static void main(String[] args) {
         int[] input = {-1, 0, 1};
         int m = 0;
-        int count=0;
+        int count = 0;
         for (int i = 0; i < input.length; i++) {
-            int sum=0;
+            int sum = 0;
             for (int j = i; j < input.length; j++) {
                 sum += input[j];
                 if (sum == m) {
@@ -22,16 +22,16 @@ public class 음수가있는부분수열 {
     }
 
     private static int actualAnswer(int[] input, int m) {
-       int answer=0;
-       int sum=0;
+        int answer = 0;
+        int sum = 0;
         HashMap<Integer, Integer> hm = new HashMap<>(); //input[i], num(요게 중요하당)+1씩 증가해서 answer에 넣는다
         hm.put(0, 1); //요건 왜 넣어주지
         for (int i : input) {
             sum += i;
             if (hm.containsKey(sum - m)) { //sum,m이 둘다 영이면! 한개 추가 되겠넴!!
-                answer+= hm.get(sum-m);
+                answer += hm.get(sum - m);
             }
-            hm.put(sum, hm.getOrDefault(sum,0)+1);
+            hm.put(sum, hm.getOrDefault(sum, 0) + 1); //왜 합을 잡아넣노!! 
         }
         return answer;
     }
