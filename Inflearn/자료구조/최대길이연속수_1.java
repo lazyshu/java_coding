14f5165cbe7a0405ce04105350ca3556f3c5dbd7
seuem <lazyslug@kakao.com>
2024-12-17 02:44:57 +0900

    12/17화 2시간 30분 :(

--- Inflearn/자료구조/최대길이연속수_1.java
+++ Inflearn/자료구조/최대길이연속수_1.java
@@ -1,6 +1,9 @@
 package 자료구조;
 
+import java.time.LocalTime;
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.TreeSet;
 
 
 public class 최대길이연속수_1 {
@@ -10,26 +13,55 @@ public class 최대길이연속수_1 {
         //5
         System.out.println("answer for solution(nums1)= " + solution(nums1));
 
+
         int[] nums2 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 0, 0, 0, 0};
         //10
         System.out.println("answer for solution(nums2)= " + solution(nums2));
+
 
         int[] nums3 = {3, 3, 3, 3, 3, 3, 3, 3};
         //1
         System.out.println("answer for solution(nums3)= " + solution(nums3));
 
+
         int[] nums4 = {-3, -1, -2, 0, 3, 3, 5, 6, 2, 2, 1, 1};
         //7
         System.out.println("answer for solution(nums4)= " + solution(nums4));
+
 
         int[] nums5 = {-5, -3, -1, -4, 3, 3, 5, 6, 2, 2, 1, 1, 7};
         //3
         System.out.println("answer for solution(nums5)= " + solution(nums5));
 
+        System.out.println("actual answer: "+actualAnswer(nums1));
+        Arrays.sort(nums1);
+        System.out.println(Arrays.toString(nums1));
+    }
 
+    private static int actualAnswer(int[] nums1) {
+        int answer=0;
+        HashSet<Integer> set = new HashSet<>();
+
+        for (Integer i : nums1) {
+            set.add(i);
+        }
+        for (int i : nums1) {
+            if (set.contains(i - 1)) {
+                continue; //1-1 =0이면 스킵 작은수가 있으면 스킵한다?
+            }
+            int count=0;
+            //나보다 1이 작은 수가 없는것만...while문 돌림
+            while (set.contains(i)) {
+                count++;
+                i++;
+            }
+            answer = Math.max(count, answer);
+        }
+        return answer;
     }
 
     private static int solution(int[] nums) {
+
         Arrays.sort(nums);
         int max = 0;
         int count = 1;
@@ -44,6 +76,5 @@ public class 최대길이연속수_1 {
             }
         }
         return Math.max(count,max);
-
     }
 }
--- Inflearn/해싱And시간파싱/문서도난.java
+++ Inflearn/해싱And시간파싱/문서도난.java
@@ -1,7 +1,8 @@
 package 해싱And시간파싱;
 
 import java.time.LocalTime;
-import java.util.*;
+import java.util.Arrays;
+import java.util.TreeMap;
 
 public class 문서도난 {
     //자동으로 시간 순서대로 정렬되게 넣으려면 TreeMap을 사용하자! >_<b
@@ -12,17 +13,16 @@ public class 문서도난 {
         System.out.println("answer = " + Arrays.toString(answer));
 
 
-
     }
 
     private static String[] solution(String[] input, String times) {
-        LocalTime begin = LocalTime.parse(times.substring(0,5));
+        LocalTime begin = LocalTime.parse(times.substring(0, 5));
         LocalTime end = LocalTime.parse(times.substring(6));
-        TreeMap<LocalTime,String> employees = new TreeMap<>();
+        TreeMap<LocalTime, String> employees = new TreeMap<>();
         for (String string : input) {
             LocalTime enteredAt = LocalTime.parse(string.substring(string.length() - 5));
-            if (begin.isBefore(enteredAt.plusMinutes(1))&&end.isAfter(enteredAt.minusMinutes(1))) {
-                employees.put(enteredAt,string.substring(0, string.length() - 5));
+            if (begin.isBefore(enteredAt.plusMinutes(1)) && end.isAfter(enteredAt.minusMinutes(1))) {
+                employees.put(enteredAt, string.substring(0, string.length() - 5));
             }
         }
         return employees.values().toArray(new String[0]);
