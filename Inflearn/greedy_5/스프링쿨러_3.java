e27524e8af5ba1a1605334de997444f00ee117bf
seuem <lazyslug@kakao.com>
2025-02-12 01:47:00 +0900

    정답 어렵다아

--- Inflearn/greedy_5/스프링쿨러_3.java
+++ Inflearn/greedy_5/스프링쿨러_3.java
@@ -1,5 +1,7 @@
 package greedy_5;
 
+import java.util.Arrays;
+
 public class 스프링쿨러_3 {
     public static void main(String[] args) {
         int n1 = 8;
@@ -15,13 +17,50 @@ public class 스프링쿨러_3 {
         int[] nums3 = {1, 2, 3, 1, 2, 1, 1, 2, 1, 1, 1, 1};
         int answer3 = 3;
 
+        int n4 = 5;
+        int[] nums4 = {1, 0,0,0, 1};
+        int answer4 = -1;
+
         solution(nums1);
         solution(nums2);
         solution(nums3);
+        solution(nums4);
 
 
+        System.out.println("actualSolution(nums1) = " + actualSolution(nums1));
+        System.out.println("actualSolution(nums2) = " + actualSolution(nums2));
+        System.out.println("actualSolution(nums3) = " + actualSolution(nums3));
+        System.out.println("actualSolution(nums4) = " + actualSolution(nums4));
 
 
+    }
+//    int[] nums2 = {1, 2, 2, 0, 0}; //0,1,2,3,4
+    private static int actualSolution(int[] nums) {
+        int count=0;
+        int[][] arr = new int[nums.length][2];
+        for (int i = 0; i < nums.length; i++) {
+            arr[i][0] = Math.max(0, i - nums[i]); //left
+            arr[i][1] = Math.min(nums.length-1, i + nums[i]); //right
+        }
+        Arrays.sort(arr,(a,b)->a[0]-b[0]); //왼쪽 범위를 기준으로 정렬..
+
+        int start=0,end=0,i=0;
+        while (i < arr.length) {
+            while (i < arr.length && arr[i][0] <= start) {
+                end = Math.max(end, arr[i][1]);
+                i++;
+            }
+            count++;
+            if (end >= arr.length-1) {
+                return count;
+            }
+            if (start == end) {
+                return -1;
+            }
+
+            start=end;
+        }
+        return count;
     }
 
     private static void solution(int[] nums) {
@@ -40,6 +79,10 @@ public class 스프링쿨러_3 {
                     }
                 }
             }
+        if (left == right) {
+            count=-1;
+            break;
+        }
             left = right; // The codes above determines how far we can move to right while still covering the left index.
             //update left to the farthest position we reached(right)
             count++;
